60004439446d08ce1a7a9765533832c52f93e7f5
Fajar Riza Fahlevi <fajarrizafahlevi@gmail.com>
2023-03-14 15:48:02 +0700

    add more item to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,6 +3,18 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer>
+      <section>
+        <div
+          style={{
+            width: '160px',
+            height: '120px',
+            borderRadius: '16px',
+            backgroundColor: 'var(--background-primary)',
+          }}
+        />
+        <h4>Jappar Draws</h4>
+        <p>2D character illustration</p>
+      </section>
       <p>Jappar Draws &copy; 2023</p>
     </footer>
   );
