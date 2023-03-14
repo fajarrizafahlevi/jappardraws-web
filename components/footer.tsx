b98294b4d92c73400de1e535f0323eae836a57d2
Fajar Riza Fahlevi <fajarrizafahlevi@gmail.com>
2023-03-14 16:39:20 +0700

    remove footer css module

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import styles from '@/styles/Footer.module.css';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer>
-      <section className={styles.container}>
-        <div className={styles.item}>
-          <div
-            style={{
-              width: '160px',
-              height: '120px',
-              borderRadius: '16px',
-              backgroundColor: 'var(--background-primary)',
-            }}
-          />
-          <h4>Jappar Draws</h4>
-          <p>2D character illustration</p>
-        </div>
-      </section>
       <p>Jappar Draws &copy; 2023</p>
+      <Link href="/tos">Terms of Service</Link>
     </footer>
   );
 }
