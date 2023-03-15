69a14d32bd48c4f15c8b6943dfc1857e59ed1e63
Fajar Riza Fahlevi <fajarrizafahlevi@gmail.com>
2023-03-15 08:01:20 +0700

    add order section

--- components/home/Order.tsx
+++ components/home/Order.tsx
@@ -1,5 +1,17 @@
 import styles from '@/styles/pages/home/Order.module.css';
 
 export default function Order() {
-  return <div>Order</div>;
+  return (
+    <section className={styles.order}>
+      <h2>How to order</h2>
+      <ol className={styles.list}>
+        <li>Message me and tell me what you need to draw about your character</li>
+        <li>
+          Please provide the references of your character, including the outfit and the pose if
+          possible
+        </li>
+        <li>When all the requirements above are met, I will start working from the sketch</li>
+      </ol>
+    </section>
+  );
 }
