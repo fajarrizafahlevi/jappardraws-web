3814300a645bf1a455edd57516c87abfec72d4d6
Fajar Riza Fahlevi <fajarrizafahlevi@gmail.com>
2023-03-18 10:10:41 +0700

    add detailed price

--- components/home/Services.tsx
+++ components/home/Services.tsx
@@ -3,17 +3,32 @@ export default function Service() {
     {
       id: 1,
       style: 'Color Sketch',
-      price: '80K IDR / 15 USD',
+      price: {
+        headShot: '80K IDR / 15 USD',
+        bustUp: '120K IDR / 20 USD',
+        halfBody: '160K IDR / 25 USD',
+        fullBody: '240K IDR / 35 USD',
+      },
     },
     {
       id: 2,
       style: 'Cel Shade',
-      price: '80K IDR / 15 USD',
+      price: {
+        headShot: '80K IDR / 15 USD',
+        bustUp: '120K IDR / 20 USD',
+        halfBody: '160K IDR / 25 USD',
+        fullBody: '240K IDR / 35 USD',
+      },
     },
     {
       id: 3,
       style: 'Full Render',
-      price: '100K IDR / 20 USD',
+      price: {
+        headShot: '100K IDR / 20 USD',
+        bustUp: '150K IDR / 25 USD',
+        halfBody: '200K IDR / 30 USD',
+        fullBody: '300K IDR / 40 USD',
+      },
     },
   ];
 
@@ -24,13 +39,16 @@ export default function Service() {
         {menu.map((item) => (
           <div
             key={item.id}
-            className="relative flex justify-center gap-8"
+            className="container w-60 h-80 p-8 rounded bg-gray-900 flex flex-col justify-end items-center hover:cursor-pointer"
           >
-            <div className="w-60 h-80 rounded bg-gray-900" />
-            <div className="absolute bottom-8">
-              <h3 className="font-bold text-yellow-300">{item.style}</h3>
-              <p>Start from</p>
-              <span>{item.price}</span>
+            <div>
+              <h3 className="font-bold text-yellow-300 mb-3">{item.style}</h3>
+              <div className="text-xs">
+                <p>Head-shot: {item.price.headShot}</p>
+                <p>Bust-up: {item.price.bustUp}</p>
+                <p>half-body: {item.price.halfBody}</p>
+                <p>Full-body: {item.price.fullBody}</p>
+              </div>
             </div>
           </div>
         ))}
