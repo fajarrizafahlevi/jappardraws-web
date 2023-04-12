595508c6da4cbc621a33c43c54905f73ebb27de0
Fajar Riza Fahlevi <fajarrizafahlevi@gmail.com>
2023-04-12 17:13:31 +0700

    add images to works page

--- pages/works.tsx
+++ pages/works.tsx
@@ -1,17 +1,68 @@
+import Image from 'next/image';
+
 export default function Works() {
+  const works = {
+    colorSketch: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'],
+    celShade: ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'],
+    fullRender: ['1.jpg', '2.jpg', '3.jpg'],
+  };
+
   return (
-    <section className="min-h-screen pb-0">
-      <h2>My Works</h2>
+    <section>
+      <h2>Works</h2>
+
+      <h3>Color Sketch</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="w-60 h-80 rounded bg-gray-900" />
-        <div className="w-60 h-80 rounded bg-gray-900" />
-        <div className="w-60 h-80 rounded bg-gray-900" />
-        <div className="w-60 h-80 rounded bg-gray-900" />
-        <div className="w-60 h-80 rounded bg-gray-900" />
-        <div className="w-60 h-80 rounded bg-gray-900" />
-        <div className="w-60 h-80 rounded bg-gray-900" />
-        <div className="w-60 h-80 rounded bg-gray-900" />
-        <div className="w-60 h-80 rounded bg-gray-900" />
+        {works.colorSketch.reverse().map((image, index) => (
+          <div
+            key={index}
+            className="relative container w-60 h-80 rounded bg-gray-900 overflow-hidden"
+          >
+            <Image
+              src={`/images/works/colorSketch/${image}`}
+              alt="sample"
+              width={240}
+              height={320}
+              className="absolute w-full h-full object-cover object-top"
+            />
+          </div>
+        ))}
+      </div>
+
+      <h3>Cel Shade</h3>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {works.celShade.reverse().map((image, index) => (
+          <div
+            key={index}
+            className="relative container w-60 h-80 rounded bg-gray-900 overflow-hidden"
+          >
+            <Image
+              src={`/images/works/celShade/${image}`}
+              alt="sample"
+              width={240}
+              height={320}
+              className="absolute w-full h-full object-cover object-top"
+            />
+          </div>
+        ))}
+      </div>
+
+      <h3>fullRender</h3>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {works.fullRender.reverse().map((image, index) => (
+          <div
+            key={index}
+            className="relative container w-60 h-80 rounded bg-gray-900 overflow-hidden"
+          >
+            <Image
+              src={`/images/works/fullRender/${image}`}
+              alt="sample"
+              width={240}
+              height={320}
+              className="absolute w-full h-full object-cover object-top"
+            />
+          </div>
+        ))}
       </div>
     </section>
   );
