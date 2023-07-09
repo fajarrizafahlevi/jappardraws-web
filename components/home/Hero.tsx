65826d9ae8b91b85f15e3aad983baf4532e74fd8
Fajar Riza Fahlevi <105531990+fajarrizafahlevi@users.noreply.github.com>
2023-07-10 06:35:30 +0700

    Add hero image and update recent image

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,18 +1,23 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import jappar from '@/public/images/jappar.png';
+import hero from '@/public/images/hero.png';
 
 export default function Hero() {
   return (
     <section
-      className="px-8 md:px-16 lg:px-20 pb-0 bg-black items-start"
+      className="px-8 md:px-16 lg:px-20 pb-0 bg-black md:flex-row-reverse items-center "
       style={{ height: '80vh' }}
     >
+      <Image 
+        src={hero}
+        width={200}
+        alt='Jappar Draws'
+      />
       <div className="bg-gradient-to-t from-black flex flex-col gap-3">
         <article className="flex flex-col gap-2">
           <div>
-            <h2 className="text-4xl">Manifest your</h2>
-            <span className="font-bold text-5xl text-yellow-300">Imagination</span>
+            <h2 className="text-3xl sm:text-4xl">Manifest your</h2>
+            <span className="font-bold text-4xl sm:text-5xl text-yellow-300">Imagination</span>
           </div>
           <p>Bring your favorite character into anime-styled 2D illustration</p>
         </article>
