d1876c407517f3c5d2a48c5d76c98aa5a3698b21
Fajar Riza Fahlevi <105531990+fajarrizafahlevi@users.noreply.github.com>
2023-07-10 08:14:13 +0700

    Adjust hero content for portrait and landscape orientation

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -5,17 +5,17 @@ import hero from '@/public/images/hero.png';
 export default function Hero() {
   return (
     <section
-      className="relative px-8 md:px-16 lg:px-20 pb-0 bg-black md:flex-row-reverse justify-end md:justify-center items-center "
+      className="relative px-8 md:px-16 lg:px-20 pb-0 bg-black landscape:flex-row-reverse justify-end md:justify-center items-center "
       style={{ height: '80vh' }}
     >
       <Image 
         src={hero}
         height={700}
         alt='Jappar Draws'
-        className='absolute px-8 md:h-4/5 md:w-auto md:px-0 md:static'
+        className='absolute px-8 md:h-2/3 md:w-auto md:px-0 md:static'
       />
       <div className='absolute z-10 w-full h-full bg-gradient-to-t from-black md:hidden'/>
-      <div className="static z-10 flex flex-col gap-3">
+      <div className="static z-10 flex flex-col gap-3 portrait:text-center">
         <article className="flex flex-col gap-2">
           <div>
             <h2 className="text-3xl sm:text-4xl">Manifest your</h2>
