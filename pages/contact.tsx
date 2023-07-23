a34f800f298fb01df3cdb956effed6e53584b4fb
Fajar Riza Fahlevi <105531990+fajarrizafahlevi@users.noreply.github.com>
2023-07-23 12:34:16 +0700

    Change email color on contact to lime

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 export default function Contact() {
   return (
@@ -11,11 +11,18 @@ export default function Contact() {
         <h2>Ready to order?</h2>
         <ol className="list-disc pl-4">
           <li>
-            Email me at <span className="text-yellow-300">jappardraws@gmail.com</span>, tell me what
-            I need to draw about your character
+            Email me at{" "}
+            <span className="text-lime-400">jappardraws@gmail.com</span>, tell
+            me what I need to draw about your character
           </li>
-          <li>Provide the references of your character, including the pose and the outfit</li>
-          <li>When all the requirements above are met, I will start working from the sketch</li>
+          <li>
+            Provide the references of your character, including the pose and the
+            outfit
+          </li>
+          <li>
+            When all the requirements above are met, I will start working from
+            the sketch
+          </li>
         </ol>
       </section>
     </>
