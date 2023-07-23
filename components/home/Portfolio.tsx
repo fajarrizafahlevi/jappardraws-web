9dcfc550e23af5fb7d71da92b1fa6bd098b78b7a
Fajar Riza Fahlevi <105531990+fajarrizafahlevi@users.noreply.github.com>
2023-07-23 11:48:07 +0700

    Update latest work section
    - Change section name to latest work
    - Add more images
    - Change grid layout

--- components/home/Portfolio.tsx
+++ components/home/Portfolio.tsx
@@ -1,17 +1,29 @@
-import recent from '@/public/images/recent.jpg';
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Portfolio() {
+  const works = {
+    latest: ["1.jpg", "2.jpg", "3.jpg", "4.jpg"],
+  };
+
   return (
     <section>
-      <h2>Recent Work</h2>
-      <Image
-        src={recent}
-        alt="recent"
-        width={240}
-        height={320}
-        className="rounded"
-      />
+      <h2>Latest Works</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        {works.latest.map((image, index) => (
+          <div
+            key={index}
+            className="relative container w-60 h-60 rounded bg-gray-900 overflow-hidden"
+          >
+            <Image
+              src={`/images/latest/${image}`}
+              alt="sample"
+              width={240}
+              height={240}
+              className="absolute w-full h-full object-cover object-top"
+            />
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
